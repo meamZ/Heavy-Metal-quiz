ab277826afe09a7cb18531f11e3b21bbbec3a7b4
Michael Z <m.zinsmeister@live.de>
2017-08-07 17:03:15 +0200

    Added the 10 Questions as a comment in the app.js file

---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+  Questions:
+  1. When was Metallica formed?
+    (r)1981
+    ---1977
+    ---1985
+    ---1979
+  2. Who is currently a Guns and Roses member
+    (r)Richard Fortus
+    ---Robin Finck
+    ---DJ Ashba
+    ---Tommy Stinson
+  3. What is NOT a Slipknot song
+    (r)My Nemesis
+    ---Duality
+    ---Surfacing
+    ---Eyeless
+  4. When was the Album 'Way of the fist' by Five Finger Death Punch released
+    (r)2007
+    ---2001
+    ---2014
+    ---2004
+  5. In which Band does Zoltan Bathory play
+    (r)Five Finger Death Punch
+    ---Slipknot
+    ---System of a Down
+    ---Disturbed
+  6. On which Album was 'Numb' by Linkinpark
+    (r)Meteora
+    ---The Hunting Party
+    ---Hybrid Theory
+    --One More Light
+  7. Whats the name of the current Metallica bassist
+    (r)Rob Trujillo
+    ---Chris Kael
+    ---Lars Ulrich
+    ---Dave Farell
+  8. What is the second Motörhead Album
+    (r)Overkill
+    ---Iron Fist
+    ---Ace of spades
+    ---Bomber
+  9. When was 'The Number of the Beast' released
+    (r)1982
+    ---1990
+    ---1980
+    ---1978
+  10. Which was the first Megadeath album that made it into the Top 10 of the Billboard Charts
+    (r)Countdown to Extinction
+    ---Peace Sells... but Who's Buying?
+    ---The World Needs a Hero
+    ---	Youthanasia
+ */
